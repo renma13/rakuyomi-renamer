575822cfb4ebc07d4ce77906fab7e27ff08d216a
renma13 <111034281+renma13@users.noreply.github.com>
2026-08-21 16:04:27 +0800

    v0.1
    First release of the plugin. Copies the downloaded Rakuyomi files and places them to a separate rakuyomi-renamed folder with the appropriate metadata applied.

---
+++ rakuyomi-renamer.koplugin/_meta.lua
@@ -0,0 +1,7 @@
+local _ = require("gettext")
+
+return {
+    name = "rakuyomi-renamer",
+    fullname = _("Rakuyomi Renamer"),
+    description = _([[Export friendly-named copies of Rakuyomi-downloaded CBZ chapters using Rakuyomi's cached manga and chapter metadata.]]),
+}
