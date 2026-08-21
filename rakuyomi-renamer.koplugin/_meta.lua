1c3da249b7be3127bdcc56c10b118b437aaa7018
renma13 <111034281+renma13@users.noreply.github.com>
2026-08-21 16:59:13 +0800

    Delete rakuyomi-renamer.koplugin directory

--- rakuyomi-renamer.koplugin/_meta.lua
+++
@@ -1,7 +0,0 @@
-local _ = require("gettext")
-
-return {
-    name = "rakuyomi-renamer",
-    fullname = _("Rakuyomi Renamer"),
-    description = _([[Export friendly-named copies of Rakuyomi-downloaded CBZ chapters using Rakuyomi's cached manga and chapter metadata.]]),
-}
